2ece7ead594b02290cd1a707ef3173607166cab5
yell <yell@gmx.de>
2024-01-21 16:25:13 +0100

    intial commit

---
+++ summarizer.py
@@ -0,0 +1,8 @@
+
+# Open a text file in read mode
+file_path = 'sources/fh-scenario-book-2-21.txt'
+with open(file_path, 'r') as file:
+    # Perform operations on the file here
+    content = file.read()
+    print(content)
+
